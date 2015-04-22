8a822e6b4b0767afc6dea3104ccd975b8e31023b
CamiloG <cjgomez@autistici.org>
2015-04-22 12:42:39 -0300

    Añadidos comentarios al código

--- src/CaptureQRBallot_Reader.java
+++ src/CaptureQRBallot_Reader.java
@@ -16,21 +16,28 @@ public class CaptureQRBallot_Reader extends Window {
     public CaptureQRBallot_Reader() {
         super("Ballot Casting");
 
+        // Add button to cast ballot
         addComponent(new Button("Cast Ballot", () -> {
+            // Retrieve ID of the voter to verify signature later
             String voterId = TextInputDialog.showTextInputBox(getOwner(), "Voter ID", "Type the ID of the Voter", "", 10);
+
+            // Read QR-Code with the encrypted ballot + signature
             String encryptedBallotWithSignature = TextInputDialog.showTextInputBox(getOwner(), "Cast Ballot", "Read QR-Code", "", 1000);
 
             try {
+                // Check signature and store (cast) encrypted ballot
                 procedure(voterId, encryptedBallotWithSignature);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
+            // Final message in case of success
             MessageBox.showMessageBox(getOwner(), "Finalizado", "Su voto ha sido guardado");
         }));
 
+        // Add button to finalize application
         addComponent(new Button("Exit application", () -> {
-            // Salirse del window
+            // Close window properly and finalize application
             getOwner().getScreen().clear();
             getOwner().getScreen().refresh();
             getOwner().getScreen().setCursorPosition(0, 0);
@@ -42,44 +49,51 @@ public class CaptureQRBallot_Reader extends Window {
 
     static public void main(String[] args) throws IOException {
 
+        // Create window to display options
         CaptureQRBallot_Reader myWindow = new CaptureQRBallot_Reader();
         GUIScreen guiScreen = TerminalFacade.createGUIScreen();
         Screen screen = guiScreen.getScreen();
 
+        // Start and configuration of the screen
         screen.startScreen();
         guiScreen.showWindow(myWindow, GUIScreen.Position.CENTER);
         screen.refresh();
+
+        // Stopping screen at finalize application
         screen.stopScreen();
 
     }
 
     static private void procedure(String voterId, String encryptedBallotWithSignature) throws IOException {
 
+        // Separate text from ballot in: length of ballot(sep) + ballot + signature, and create BigInteger ballot and byte[] signature
         int sep = Integer.parseInt(encryptedBallotWithSignature.substring(0, 3));
-
         BigInteger ballot = new BigInteger(encryptedBallotWithSignature.substring(3, sep+3));
         byte[] signature = new BigInteger(encryptedBallotWithSignature.substring(sep+3)).toByteArray();
 
         // Verify signature
         try {
             if (!verifySign(signature, ballot, voterId)) {
-                // Hacer algo si la firma es inválida
+                // TODO: Do something if signature is invalid
                 System.exit(0);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        // TODO: Verificar que no haya votado dos veces
+        // TODO: Verify that the voter hadn't had casted already a ballot
 
+        // Asign random number to the vote casted
         int random = new Random().nextInt();
         random = random > 0 ? random : random*-1;
-        // TODO: setear random buscando que no haya sido utilizado antes
+        // TODO: set random to a number never used before
 
-        ObjectOutputStream encryptedBallotFile;
+        // Create directory for ballots if it's not created
         File dir1 = new File("ballots");
         dir1.mkdir();
 
+        // Save ballot in a file with a random number as name
+        ObjectOutputStream encryptedBallotFile;
         try {
             encryptedBallotFile = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("ballots/" + random)));
             encryptedBallotFile.writeObject(ballot);
@@ -91,18 +105,21 @@ public class CaptureQRBallot_Reader extends Window {
 
     }
 
+    // Function to verify signature within the ballot
     static private boolean verifySign(byte[] sign, BigInteger message, String id) throws IOException, ClassNotFoundException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
 
-        PublicKey publicKey;
+        // Retrieve public key of the voter from the folder publicKeys/
         ObjectInputStream oin_public = new ObjectInputStream(new BufferedInputStream(new FileInputStream("publicKeys/" + id + "publicKey.key")));
-        publicKey = (PublicKey) oin_public.readObject();
+        PublicKey publicKey = (PublicKey) oin_public.readObject();
 
+        // Set-up scheme of signature, this case is SHA1 with RSA
         Signature signature = Signature.getInstance("SHA1withRSA");
-        signature.initVerify(publicKey);
 
+        // Set-up of the verification of the signature, giving the public key and the message
+        signature.initVerify(publicKey);
         signature.update(message.toByteArray());
 
-        // Se comprueba la veracidad de la firma
+        // Checks the veracity of the signature, returning true or false
         return signature.verify(sign);
 
     }
