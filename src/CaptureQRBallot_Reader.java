adf421d159a23011c22af6abe30a94eaf7e0b4c7
CamiloG <cjgomez@autistici.org>
2015-05-29 17:21:53 -0300

    - Ballot se envía al BB montado con Ruby on Rails, en formato JSON. - Llave pública debería descargarse del BB en formato JSON, pero falta aun el desarrollo de esta parte. - Ya no se deja registro local, ni de los votos ni de las llaves públicas de los votantes.

--- src/CaptureQRBallot_Reader.java
+++ src/CaptureQRBallot_Reader.java
@@ -8,13 +8,17 @@ import com.googlecode.lanterna.screen.Screen;
 
 import java.io.*;
 import java.math.BigInteger;
-import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.security.*;
-import java.util.Random;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 public class CaptureQRBallot_Reader extends Window {
+
+    private static final String bbServer = "http://cjgomez.duckdns.org:3000/ballots";
+    private static final String publicKeysServer = "http://cjgomez.duckdns.org:3000/public_keys";
 
     public CaptureQRBallot_Reader() {
         super("Ballot Casting");
@@ -24,11 +28,11 @@ public class CaptureQRBallot_Reader extends Window {
             // Retrieve ID of the voter to verify signature later
             String voterId = TextInputDialog.showTextInputBox(getOwner(), "Voter ID", "Type the ID of the Voter", "", 10);
 
-            // Read QR-Code with the encrypted ballot + signature
+            // Read QR-Code with the encrypted vote + signature
             String encryptedBallotWithSignature = TextInputDialog.showTextInputBox(getOwner(), "Cast Ballot", "Read QR-Code", "", 1000);
 
             try {
-                // Check signature and store (cast) encrypted ballot
+                // Check signature and upload (cast) encrypted vote
                 procedure(voterId, encryptedBallotWithSignature);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -71,31 +75,39 @@ public class CaptureQRBallot_Reader extends Window {
 
         // Separate text from ballot in: length of ballot(sep) + ballot + signature, and create BigInteger ballot and byte[] signature
         int sep = Integer.parseInt(encryptedBallotWithSignature.substring(0, 3));
-        BigInteger ballot = new BigInteger(encryptedBallotWithSignature.substring(3, sep+3));
-        byte[] signature = new BigInteger(encryptedBallotWithSignature.substring(sep+3)).toByteArray();
+        String encryptedBallotString = encryptedBallotWithSignature.substring(3, sep+3);
+        String signatureString = encryptedBallotWithSignature.substring(sep+3);
+
+        // Create the BigInteger ballot and byte[] signature
+        BigInteger ballot = new BigInteger(encryptedBallotString);
+        byte[] signature = new BigInteger(signatureString).toByteArray();
+
+        // Download Public Key of the voter from the BB
+        String publicKeyString = downloadPublicKey(publicKeysServer, voterId);
 
         // Verify signature
-        try {
+        /*try {
             if (!verifySign(signature, ballot, voterId)) {
                 // TODO: Do something if signature is invalid
                 System.exit(0);
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
 
         // TODO: Verify that the voter hadn't had casted already a ballot
 
         // Asign random number to the vote casted
-        int random = new Random().nextInt();
-        random = random > 0 ? random : random*-1;
+        // int random = new Random().nextInt();
+        // random = random > 0 ? random : random*-1;
         // TODO: set random to a number never used before
 
         // Create directory for ballots if it's not created
-        File dir1 = new File("ballots");
-        dir1.mkdir();
+        // File dir1 = new File("ballots");
+        // dir1.mkdir();
 
         // Save ballot in a file with a random number as name
+        /*
         ObjectOutputStream encryptedBallotFile;
         try {
             encryptedBallotFile = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("ballots/" + random)));
@@ -104,19 +116,28 @@ public class CaptureQRBallot_Reader extends Window {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        */
 
-        // Upload to the BB of the ballot just casted
-        File ballotFile = new File("ballots/" + random);
-        upload("172.30.65.162:12345", "user", "pass", "ballots/ballot_" + random, ballotFile);
+        // Upload (cast) to the BB of the ballot
+        upload(bbServer, voterId, encryptedBallotString, signatureString);
 
     }
 
     // Function to verify signature within the ballot
-    static private boolean verifySign(byte[] sign, BigInteger message, String id) throws IOException, ClassNotFoundException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+    static private boolean verifySign(byte[] sign, BigInteger message, String id) throws IOException, ClassNotFoundException, NoSuchAlgorithmException, InvalidKeySpecException, SignatureException, InvalidKeyException {
+
+        // Download Public Key of the voter from the BB
+        String publicKeyString = downloadPublicKey(publicKeysServer, id);
+
+        // Decodify the String and create the variable PublicKey
+        byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyString.getBytes("utf-8"));
+        X509EncodedKeySpec publicSpec = new X509EncodedKeySpec(publicKeyBytes);
+        KeyFactory publicKeyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = publicKeyFactory.generatePublic(publicSpec);
 
         // Retrieve public key of the voter from the folder publicKeys/
-        ObjectInputStream oin_public = new ObjectInputStream(new BufferedInputStream(new FileInputStream("publicKeys/" + id + "publicKey.key")));
-        PublicKey publicKey = (PublicKey) oin_public.readObject();
+        // ObjectInputStream oin_public = new ObjectInputStream(new BufferedInputStream(new FileInputStream("publicKeys/" + id + "publicKey.key")));
+        // PublicKey publicKey = (PublicKey) oin_public.readObject();
 
         // Set-up scheme of signature, this case is SHA256 with RSA
         Signature signature = Signature.getInstance("SHA256withRSA");
@@ -130,87 +151,53 @@ public class CaptureQRBallot_Reader extends Window {
 
     }
 
-    // Upload of the file
-    /**
-     * Upload a file to a FTP server. A FTP URL is generated with the
-     * following syntax:
-     * ftp://user:password@host:port/filePath;type=i.
-     *
-     * @param ftpServer , FTP server address (optional port ':portNumber').
-     * @param user , Optional user name to login.
-     * @param password , Optional password for user.
-     * @param fileName , Destination file name on FTP server (with optional
-     *            preceding relative path, e.g. "myDir/myFile.txt").
-     * @param source , Source file to upload.
-     * @throws IOException on error.
-     */
-    public static void upload( String ftpServer, String user, String password,
-                        String fileName, File source ) throws IOException
-    {
-        if (ftpServer != null && fileName != null && source != null)
-        {
-            StringBuffer sb = new StringBuffer( "ftp://" );
-            // check for authentication else assume its anonymous access.
-            if (user != null && password != null)
-            {
-                sb.append( user );
-                sb.append( ':' );
-                sb.append( password );
-                sb.append( '@' );
-            }
-            sb.append( ftpServer );
-            sb.append( '/' );
-            sb.append( fileName );
-         /*
-          * type ==&gt; a=ASCII mode, i=image (binary) mode, d= file directory
-          * listing
-          */
-            sb.append( ";type=i" );
+    // Retrieve JSON of the PublicKey from the BB
+    static private String downloadPublicKey(String publicKeyServer, String voterId) throws IOException {
+        // Set the URL to GET the public key of the voterId
+        String url = publicKeyServer + "/voter/" + voterId;
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-            BufferedInputStream bis = null;
-            BufferedOutputStream bos = null;
-            try
-            {
-                URL url = new URL( sb.toString() );
-                URLConnection urlc = url.openConnection();
+        // Add request header
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.getResponseCode();
 
-                bos = new BufferedOutputStream( urlc.getOutputStream() );
-                bis = new BufferedInputStream( new FileInputStream( source ) );
-
-                int i;
-                // read byte by byte until end of stream
-                while ((i = bis.read()) != -1)
-                {
-                    bos.write( i );
-                }
-            }
-            finally
-            {
-                if (bis != null)
-                    try
-                    {
-                        bis.close();
-                    }
-                    catch (IOException ioe)
-                    {
-                        ioe.printStackTrace();
-                    }
-                if (bos != null)
-                    try
-                    {
-                        bos.close();
-                    }
-                    catch (IOException ioe)
-                    {
-                        ioe.printStackTrace();
-                    }
-            }
+        // Receive the response
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
         }
-        else
-        {
-            System.out.println( "Input not available." );
-        }
+        in.close();
+
+        // TODO: Process the response as a JSON object
+
+        return "TODO";
+
     }
 
+    // Upload of the ballot as a JSON to the bbServer
+    static private void upload(String bbServer, String voterId, String encryption, String signature) throws IOException {
+        // Set the URL where to POST the ballot
+        URL obj = new URL(bbServer);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        //add request header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+
+        // Create JSON with the parameters
+        String urlParameters = "{\"ballot\":{\"voter\":" + voterId + ",\"enc\":" + encryption + ",\"sign\":" + signature + "}}";
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+        con.getResponseCode();
+    }
 
 }
